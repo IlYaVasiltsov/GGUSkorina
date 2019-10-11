ab45daaf49259d0fcda8969bb586aa8a5c1b7f61
IlYaVasiltsov <55573476+IlYaVasiltsov@users.noreply.github.com>
2019-10-11 15:41:29 +0300

    Add files via upload

---
+++ lab2upr2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+float a,b,c,d ;
+scanf("%f",&a);
+scanf("%f",&b);
+scanf("%f",&c);
+scanf("%f",&d);
+if (b-a==d || c-b==d) printf("TRUE");
+else printf("FALSE");
+
+
+
+
+}
