ea9a49a18943da3e5ac4f7c43621bf3e51b7147d
IlYaVasiltsov <55573476+IlYaVasiltsov@users.noreply.github.com>
2019-10-11 15:29:10 +0300

    Add files via upload

---
+++ lab2upr1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+float a,b,c ;
+scanf("%f",&a);
+scanf("%f",&b);
+scanf("%f",&c);
+if(a==b||b==c||c==a) printf("Треугольник равносторонний");
+else printf("Треугольник не равносторонний");
+
+
+
+
+}
