7b4986056e54dbf533330845d4e0f13d363d70f7
IlYaVasiltsov <55573476+IlYaVasiltsov@users.noreply.github.com>
2019-10-11 14:44:25 +0300

    Update lab1urp2.c

--- lab1urp2.c
+++ lab1urp2.c
@@ -1,22 +1,21 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>   
 
-int main(void) {
+int main () {
 
-    float a,b,c,d,u,s,x,y,z;
-    printf("Введите переменные x,y,z");
+    float a,b,c,s,x,y,z;
+    printf("Введите переменную x:");
     scanf("%f",&x);
+    printf("Введите переменную y:");
     scanf("%f",&y);
+    printf("Введите переменную z:");
     scanf("%f",&z);
-    a=exp pow (abs (x-y));
-    b=abs (x-y) pow (x+y);
-    c=atan(x);
-    d=atan(z);
-    u=pow (pow(x,6)+ pow(ln(y),2)),1/3);
-
-    s=exp pow
-    printf("Введите x=-0,02235");
+    a=exp(fabs(x-y))*pow(fabs(x-y),x+y);
+    b=atan(x)+atan(z);
+    c=pow(pow(x,6)+pow(log(y),2),1/3);
+    s=a/b+c;
+    printf("%f",s);
 return 0;
 
 }
