0212818dab3e62c01b327d3ff932040ea8e0260d
IlYaVasiltsov <55573476+IlYaVasiltsov@users.noreply.github.com>
2019-10-25 14:23:19 +0300

    Update lab2upr3.c

--- lab2upr3.c
+++ lab2upr3.c
@@ -1,20 +1,20 @@
-include<math.h>
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
 int main ()
 {
     int n;
-printf("Введите 1=+,2=-,3=/,4=* \n");
+printf("Введите 1=>+,2=>-,3=>/,4=>* \n");
 
 
 scanf("%i", &n);
 switch (n) {
-case 1: printf("7+7");
+case 1: printf("7+7=14");
 break;
-case 2: printf("7-7");
+case 2: printf("7-7=0");
 break;
-case 3: printf ("7/7");
+case 3: printf ("7/7=1");
 break;
-case 4: printf ("7*7");
+case 4: printf ("7*7=49");
 break;
 }
 return 0;
